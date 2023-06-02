b57ce3b1ba7425091b058ebea301ab4d0309cbe9
Abdulmalik <reex4des@gmail.com>
2023-06-02 14:53:19 +0300

    updat ratelimiter

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import random
 
 from config import settings
 
-app = FastAPI(dependencies=[Depends(RateLimiter(times=1, seconds=5))])
+app = FastAPI(dependencies=[Depends(RateLimiter(times=10, seconds=5))])
 logger = logging.getLogger(__name__)
 # .env variables can be validated and accessed from the config, here to set a log level
 logging.basicConfig(level=settings.LOG_LEVEL.upper())
